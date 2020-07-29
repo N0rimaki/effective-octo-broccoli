13fead2d6a047965285d4bdb6888dfbda7ebc859
Thomas <git@tomig.at>
2020-07-29 08:34:33 +0200

    just works

--- copybot.py
+++ copybot.py
@@ -1,5 +1,6 @@
 import praw
 import logging as log
+import textwrap
 
 LOG_FILENAME = "./log_.log"
 
@@ -10,25 +11,27 @@ log.basicConfig(handlers=[
 def work():
 	log.info("Lastrun of the script")
 	
-	_subtocrosspost = 'FuckZhu'
+	_subtocrosspost = 'UnpopularFacts'
 	try:
-		reddit = praw.Reddit(client_id="xxxxxxxxxx",
+		reddit = praw.Reddit(client_id="xxxxxxxxx",
 							client_secret="xxxxxxxxxxxxxx",
-							password="xxxxxxxxxxxxxx",
-							username="Karen-o-matic",
-							user_agent="copybot by /u/Karen-o-matic")
+							password="xxxxxxxxx",
+							username="unpopularcummybot",
+							user_agent="UnpopularCummyBot by /u/unpopularcummybot")
 		reddit.validate_on_submit=True	
 		
-		for submission in reddit.subreddit(_subtocrosspost).hot(limit=25):
+		for submission in reddit.subreddit(_subtocrosspost).top('all',limit=1000):
+		
+			shorten= textwrap.shorten(submission.selftext,width=990, placeholder="...")
 			
-			text = "Title:\n*"+submission.title+"*\n\nText:\n*"+submission.selftext+"*"
+			text = "Backup in case something happens to the post:\n\nTitle:\n*"+submission.title+"*\n\nText of the post:\n"+shorten
 			try:
 				modcommentid = submission.reply(text)
 				comment = reddit.comment(modcommentid)
-				comment.mod.distinguish(how="yes", sticky=True)		
-				log.info("answered {} {} {}".format(submission.id,submission.title,submission.permalink))
+				comment.mod.distinguish(how="yes")		
+				log.info("answered {};{};{}".format(submission.id,submission.title,submission.permalink))
 			except Exception as err:
-				log.error("can't answer this {} {} {} {}".format(submission.id,submission.title,submission.permalink,str(err)))
+				log.error("can't answer this {};{};{};{}".format(submission.id,submission.title,submission.permalink,str(err)))
 
 	except Exception as err:
 		log.error("Run thru all submissions {}".format(str(err)))
